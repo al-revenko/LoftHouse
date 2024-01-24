b6ef83f12c9f37bad55025f1148ae4f500b31325
Alexander Revenko <revenko.a@bk.ru>
2024-01-24 17:28:49 +0700

    Добавил маску для телефона

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-const burgerBtn = document.getElementById('burger');
-const headerTop = document.getElementById('header-top')
+
+$(document).ready(function () {
+
+  const phoneMask = ["+7 (999) 999-99-99"]
+
+  const burgerBtn = document.getElementById("burger");
+  const headerTop = document.getElementById("header-top");
+
+  burgerBtn.onclick = () => {
+    burgerBtn.classList.toggle("is-active");
+    headerTop.classList.toggle("header__top--popup");
+    document.body.style.overflowY = "hidden";
+  };
+
+  $('#phone-cta').inputmask({ mask: phoneMask }); 
+  $('#phone-feedback').inputmask({ mask: phoneMask }); 
+});
 
 
-burgerBtn.onclick = () => {
-  burgerBtn.classList.toggle('is-active');
-  headerTop.classList.toggle('header__top--popup')
-  document.body.style.overflowY = 'hidden'
-}
