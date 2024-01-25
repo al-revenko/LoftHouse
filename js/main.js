b9318e3ec07f4492b610afb233a5c95f938e511f
Alexander Revenko <revenko.a@bk.ru>
2024-01-25 16:51:31 +0700

    Добавил Яндекс карту на страницу

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-
 $(document).ready(function () {
-
-  const phoneMask = ["+7 (999) 999-99-99"]
-
   const burgerBtn = document.getElementById("burger");
   const headerTop = document.getElementById("header-top");
 
@@ -12,10 +8,63 @@ $(document).ready(function () {
     document.body.style.overflowY = "hidden";
   };
 
-  $('#phone-cta').inputmask({ mask: phoneMask }); 
-  $('#phone-feedback').inputmask({ mask: phoneMask }); 
+  initMap();
 
-  $('#video-link').modalVideo();
+  async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
+
+    const {
+      YMap,
+      YMapDefaultSchemeLayer,
+      YMapDefaultFeaturesLayer,
+      YMapMarker,
+    } = ymaps3;
+
+    // Иницилиазируем карту
+    const map = new YMap(
+      // Передаём ссылку на HTMLElement контейнера
+      document.getElementById("yt-map-api"),
+
+      // Передаём параметры инициализации карты
+      {
+        location: {
+          // Координаты центра карты
+          center: [30.338928, 59.943543],
+
+          // Уровень масштабирования
+          zoom: 16,
+        },
+      }
+    );
+
+    // Инициализируйте маркер
+    const icon = document.createElement("img");
+
+    icon.className = "map__marker-icon";
+    icon.src = "./img/svg/yt-map-point.svg";
+
+    const marker = new YMapMarker(
+      {
+        coordinates: [30.338928, 59.943543],
+      },
+      icon
+    );
+
+    map
+      .addChild(new YMapDefaultSchemeLayer())
+      .addChild(new YMapDefaultFeaturesLayer())
+      .addChild(marker);
+
+    document.querySelector(
+      ".ymaps3x0--layer.ymaps3x0--marker-layer"
+    ).style.width = "100%";
+  }
+
+  const phoneMask = ["+7 (999) 999-99-99"];
+
+  $("#phone-cta").inputmask({ mask: phoneMask });
+  $("#phone-feedback").inputmask({ mask: phoneMask });
+
+  $("#video-link").modalVideo();
 });
-
-
